836bfe1f478c1135c180738f85bce50813e41310
Refinery <refinery@goldenpipeline.com>
2020-07-25 19:55:44 -0500

    Add support for build all applications and deploy once

--- .scripts/bld.sh
+++ .scripts/bld.sh
@@ -1,13 +1,31 @@
 PRODUCT_NAME="rgb"
-MODULE_NAME="$1"
-IMAGE="$PRODUCT_NAME-$MODULE_NAME:`uuidgen`"
 
-echo "🔵 Building Dockerfile for module..."
-docker build -t $IMAGE ./$MODULE_NAME
+function deploy {
+  echo "🔵 Deploying umbrella chart..."
+  kubectl create namespace $PRODUCT_NAME
+  helm upgrade $PRODUCT_NAME ./charts --install --namespace $PRODUCT_NAME
+}
 
-echo "🔵 Updating module chart with new image..."
-.scripts/update-chart-image.sh $MODULE_NAME $IMAGE
+function build {
+  local MODULE_NAME="$1"
+  local IMAGE="$PRODUCT_NAME-$MODULE_NAME:`uuidgen`"
 
-echo "🔵 Deploying umbrella chart..."
-kubectl create namespace $PRODUCT_NAME
-helm upgrade $PRODUCT_NAME ./charts --install --namespace $PRODUCT_NAME
+  echo "🔵 Building Dockerfile for module..."
+  docker build -t $IMAGE ./$1
+
+  echo "🔵 Updating module chart with new image..."
+  .scripts/update-chart-image.sh $1 $IMAGE
+}
+
+if [[ $1 -eq "all" ]]
+then
+  build 'frontend-web'
+  build 'goal-data-service'
+  build 'goal-device-service'
+  build 'goal-event-processor'
+  build 'goal-event-service'
+else
+  build $1
+fi
+
+deploy
